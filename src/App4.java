0f84ff2dc94e5a0e1bc53c08deaf7b85968f5a76
Giovanni Cappelletti <tech@methodwolf.com>
2024-06-18 14:26:57 +0200

    update

---
+++ src/App4.java
@@ -0,0 +1,128 @@
+public class App4 {
+
+    /**
+     * Esercizio 1: Conto alla rovescia
+     * Argomenti trattati: variabili, primitivi, Scanner, syso, loop
+     * 
+     * Scrivi un programma che esegua un conto alla rovescia da un numero
+     * specificato dall'utente a 0. Il programma dovrebbe:
+     * 
+     * Chiedere all'utente di inserire un numero intero positivo utilizzando la
+     * classe Scanner.
+     * Utilizzare un ciclo for per contare alla rovescia.
+     * Stampare ogni numero durante il conto alla rovescia.
+     * 
+     * ------------------------------------------------------------------------------------------------
+     * 
+     * Esercizio 2: Tabelline
+     * Argomenti trattati: variabili, primitivi, Scanner, syso, loop
+     * 
+     * Scrivi un programma che stampi la tabellina di un numero specificato
+     * dall'utente. Il programma dovrebbe:
+     * 
+     * Chiedere all'utente di inserire un numero intero utilizzando la classe
+     * Scanner.
+     * Utilizzare un ciclo for per stampare la tabellina del numero inserito
+     * dall'utente.
+     * Stampare i risultati della tabellina.
+     * 
+     * ------------------------------------------------------------------------------------------------
+     * 
+     * Esercizio 3: Ricerca in un array
+     * Argomenti trattati: variabili, primitivi, Scanner, syso, array, loop
+     * 
+     * Scrivi un programma che cerchi un numero specificato dall'utente in un array
+     * di numeri interi. Il programma dovrebbe:
+     * 
+     * Creare un array di numeri interi con valori random compresi tra 0 e 99
+     * Chiedere all'utente di inserire un numero intero utilizzando la classe
+     * Scanner.
+     * Utilizzare un ciclo for per cercare il numero nell'array.
+     * Stampare un messaggio che indica se il numero è presente nell'array.
+     * 
+     * N.B.: stampare l'array per effettuare il controllo manuale
+     * 
+     * ------------------------------------------------------------------------------------------------
+     * 
+     * Esercizio 4: Conta caratteri specifici
+     * Argomenti trattati: variabili, Scanner, syso, stringhe, loop
+     * 
+     * Scrivi un programma che conti il numero di volte che un carattere specificato
+     * dall'utente appare in una stringa. Il programma dovrebbe:
+     * 
+     * Chiedere all'utente di inserire una stringa e un carattere utilizzando la
+     * classe Scanner.
+     * Utilizzare un ciclo for per contare le occorrenze del carattere nella
+     * stringa.
+     * Stampare il conteggio delle occorrenze.
+     * 
+     * ------------------------------------------------------------------------------------------------
+     * 
+     * Esercizio 5: Stampa caratteri alternativi
+     * Argomenti trattati: variabili, Scanner, syso, stringhe, loop
+     * 
+     * Scrivi un programma che stampi solo i caratteri in posizione pari di una
+     * stringa inserita dall'utente. Il programma dovrebbe:
+     * 
+     * Chiedere all'utente di inserire una stringa utilizzando la classe Scanner.
+     * Utilizzare un ciclo for per iterare attraverso la stringa.
+     * Stampare solo i caratteri in posizione pari.
+     * 
+     * ------------------------------------------------------------------------------------------------
+     * 
+     * Esercizio 6: Reverse di una stringa
+     * Argomenti trattati: variabili, Scanner, syso, stringhe, loop
+     * 
+     * Scrivi un programma che inverta una stringa inserita dall'utente. Il
+     * programma dovrebbe:
+     * 
+     * Chiedere all'utente di inserire una stringa utilizzando la classe Scanner.
+     * Utilizzare un ciclo for per costruire la stringa invertita.
+     * Stampare la stringa invertita.
+     * 
+     * ------------------------------------------------------------------------------------------------
+     * 
+     * Esercizio 7: Somma dei numeri pari in un array
+     * Argomenti trattati: variabili, primitivi, Scanner, syso, array, loop
+     * 
+     * Scrivi un programma che calcoli la somma dei numeri pari in un array di
+     * numeri interi. Il programma dovrebbe:
+     * 
+     * Chiedere all'utente di inserire la dimensione dell'array e riempire l'array
+     * di numeri random compresi tra 0 e 99.
+     * Utilizzare un ciclo for per sommare solo i numeri pari nell'array.
+     * Stampare la somma risultante.
+     */
+
+    public static void main(String[] args) {
+
+        es1();
+        // es2();
+        // es3();
+        // es4();
+        // es5();
+        // es6();
+        // es7();
+    }
+
+    public static void es1() {
+    }
+
+    public static void es2() {
+    }
+
+    public static void es3() {
+    }
+
+    public static void es4() {
+    }
+
+    public static void es5() {
+    }
+
+    public static void es6() {
+    }
+
+    public static void es7() {
+    }
+}
